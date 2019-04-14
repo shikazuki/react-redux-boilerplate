ef3759b3e1b8a75c04b13d97dc433d967d21b133
shikazuki <shikazuki0@gmail.com>
2019-04-14 19:43:04 +0900

    fix prettier rules.

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "arrowParens": "always",
+  "jsxBracketSameLine": true,
+  "printWidth": 120,
+  "semi": true,
+  "singleQuote": false,
+  "tabWidth": 2,
+  "trailingComma": "es5"
+};
