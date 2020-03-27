cdf5fc01b7d429d2a6cab6e42fd25c9917bcdcaf
shikazuki <shikazuki0@gmail.com>
2020-03-27 18:25:33 +0900

    remove all.

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "arrowParens": "always",
-  "jsxBracketSameLine": true,
-  "printWidth": 120,
-  "semi": true,
-  "singleQuote": false,
-  "tabWidth": 2,
-  "trailingComma": "es5"
-};
--- src/components/App/App.stories.tsx
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from "@storybook/react";
-import * as React from "react";
-import App from "./App";
-
-storiesOf("App", module).add("Init", () => <App />);
